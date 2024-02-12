e19d494e1f199c05eecfad2a6637a4887d5ba80f
Khush Narang <106396575+KhushNarang1@users.noreply.github.com>
2024-02-12 22:55:43 +0530

    changes

--- fund-frontend/src/component/NavBar/Items.jsx
+++ fund-frontend/src/component/NavBar/Items.jsx
@@ -1,25 +1,28 @@
 import { IoSearchSharp } from "react-icons/io5";
 
-export const Items = [
+
+export const PrimaryItems = [
     {
-        title : "How it works",
-        url : "/howItWorks",
-        cName : "nav-links",
+        title: "How it works",
+        url: "/howItWorks",
+        cName: "nav-links",
     },
     {
-        title : "FAQ",
-        url : "/faq",
-        cName : "nav-links",
+        title: "FAQ",
+        url: "/faq",
+        cName: "nav-links",
+    },
+];
+
+export const SecondaryItems = [
+    {
+        url: "/search",
+        cName: "nav-link",
+        icon: IoSearchSharp,
     },
     {
-        // title : "Search",
-        url : "/search",
-        cName : "nav-links",
-        icon : IoSearchSharp,
+        title: "Start Fundraising",
+        url: "/contact",
+        cName: "nav-link",
     },
-    {
-        title : "Start Fundraising",
-        url : "/contact",
-        cName : "nav-links",
-    },
-]
+];
